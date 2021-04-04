1bc9707a8b2e261054fcfef2d4dd1fd70116a0fd
Rafael Candia <candia.rafael1@gmail.com>
2021-04-04 20:35:32 -0300

    add example

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" :src="imageSource" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <button @click="changeImageSource('cassius')">clique</button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import VueImage from "../assets/logo.png";
 
 export default {
-  name: 'Home',
+  name: "Home",
+  data() {
+    return {
+      imageSource: VueImage,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  methods: {
+    changeImageSource(url) {
+      this.imageSource = url;
+    },
+  },
+};
 </script>
